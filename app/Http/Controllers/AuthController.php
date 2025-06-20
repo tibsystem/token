8896dbc48fa0f946553cee84c082493cdb9db471
package <wesleyamancio@Laptop-de-Wesley.local>
2025-06-20 19:09:53 -0300

    model

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -37,7 +37,7 @@ public function login(Request $request)
         $user = User::create([
             'nome' => $data['nome'],
             'email' => $data['email'],
-            'senha_hash' => hash('sha256', $data['password']),
+            'password' => bcrypt($data['password']), // ← CORRETO
             'tipo' => $data['tipo'] ?? 'investidor',
             'telefone' => $data['telefone'] ?? null,
         ]);
